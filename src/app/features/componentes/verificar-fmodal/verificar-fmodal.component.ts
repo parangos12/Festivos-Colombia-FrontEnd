af0a173348f07a08086404a9223f3e09222dab2d
personal <parangos@eafit.edu.co>
2024-06-08 16:49:09 -0500

    modal modified

--- src/app/features/componentes/verificar-fmodal/verificar-fmodal.component.ts
+++ src/app/features/componentes/verificar-fmodal/verificar-fmodal.component.ts
@@ -1,11 +1,16 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatDialogModule } from '@angular/material/dialog';
+import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
+import { ColumnMode, DatatableComponent, NgxDatatableModule,SelectionType  } from '@swimlane/ngx-datatable';
+import { Festivo } from '../../../core/entidades/Festivo';
+import { FormsModule } from '@angular/forms';
+import { FestivosService } from '../../servicios/festivos.service';
 
 @Component({
   selector: 'app-verificar-fmodal',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [ReferenciasMaterialModule,NgxDatatableModule,FormsModule,MatDialogModule],
   templateUrl: './verificar-fmodal.component.html',
   styleUrl: './verificar-fmodal.component.css'
 })
